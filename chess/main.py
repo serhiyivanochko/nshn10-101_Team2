c038026269ee6cc977c50842cf9777e82874bdf1
Nazar <nazar.sokalchuk@gmail.com>
2020-11-19 22:51:46 +0200

    queen and pawn

--- chess/main.py
+++ chess/main.py
@@ -4,13 +4,18 @@ window = Tk()
 
 def create_board():
     count_rows = 8
-    pawn_rows = 2
+    figure_count_rows = 2
+    figure_rows = 2
+    figure_count_rows_two = 1
     width = 75
     start_x = 0
     start_y = 0
     width_p = 73
     start_y_p = 0
     start_x_p = 0
+    start_x_q = 0
+    start_y_q = 0
+    width_q = 75
     cell_color = "white"
     pawn_color = "yellow"
     for i in range(count_rows):
@@ -27,13 +32,21 @@ def create_board():
             cell_color = "brown"
         else:
             cell_color = "white"
-    for k in range(pawn_rows):
+    for k in range(figure_rows):
         for e in range(count_rows):
             pawn = canvas.create_rectangle(start_x_p + 30, start_y_p + 95, start_x_p + 65, start_y_p + 130, fill = pawn_color)
             start_x_p = start_x_p + width_p
         start_y_p = start_y_p + 375
         start_x_p = 0
         pawn_color = "black"
+    for t in range(figure_count_rows):
+        for y in range(figure_count_rows_two):
+            qeen = canvas.create_rectangle(start_x_q + 325, start_y_q + 25, start_x_q + 360, start_y_q + 60, fill = pawn_color)
+            start_x_q = start_x_q + width_q
+        start_y_q = start_y_q + 525
+        start_x_q = 0
+        pawn_color = "black"
+
 
 
 
